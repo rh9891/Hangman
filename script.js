ed3de75f557555f22b0137a0ed4caed2fa27c8f4
Romie Hecdivert <65568062+rh9891@users.noreply.github.com>
2020-12-26 21:28:45 -0500

    Completed the functionality to display words. CSS styling still needs work, but application is working as expected.

--- script.js
+++ script.js
@@ -14,3 +14,31 @@ let selectedWord = words[Math.floor(Math.random() * words.length)];
 
 const correctGuesses = [];
 const incorrectGuesses = [];
+
+// Displays the new word.
+displayWord = () => {
+  wordElement.innerHTML = `
+    ${selectedWord
+      // Splits the selected word into an array.
+      .split("")
+      // Maps through it to see if the letter guessed is included in that array; Outputs the letter if it is included, but outputs an empty string if it is not.
+      .map(
+        (letter) =>
+          `<span class="letter"> ${
+            correctGuesses.includes(letter) ? letter : ""
+          }</span>`
+      )
+      // Joins the letters to revert them back to a string.
+      .join("")}
+    `;
+
+  // Eliminates new lines from being created after each character, so that its readability is better when determining a win.
+  const innerWord = wordElement.innerText.replace(/\n/g, "");
+
+  if (innerWord === selectedWord) {
+    finalMessage.innerText = "Congrats! You've won! 🎉";
+    popup.style.display = "flex";
+  }
+};
+
+displayWord();
